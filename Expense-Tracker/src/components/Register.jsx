456d1a9d5f913c668c6b5e85b1fe4e0d164b644b
dev kacha <devkacha90@gmail.com>
2025-07-03 23:44:21 +0530

    add api in register

--- Expense-Tracker/src/components/Register.jsx
+++ Expense-Tracker/src/components/Register.jsx
@@ -1,4 +1,41 @@
+import { useState } from "react";
+
+import axios from "axios";
+
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8000/api/register", formData)
+      .then((response) => {
+        console.log(response);
+        setFormData({
+          name: "",
+          email: "",
+          password: "",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const registerFormData = [
+    { name: "name", type: "text", placeholder: "John Doe" },
+    { name: "email", type: "email", placeholder: "JohnDoe@gmail.com" },
+    { name: "password", type: "password", placeholder: "••••••••" },
+  ];
   return (
     <>
       <div className="flex items-center justify-center min-h-[calc(100vh-80px)] bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark p-4">
@@ -6,67 +43,27 @@ const Register = () => {
           <h2 className="text-3xl font-bold text-center text-teal-700 mb-8">
             Register
           </h2>
-          <form className="space-y-6">
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Full Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-400"
-                placeholder="John Doe"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-400"
-                placeholder="you@example.com"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-400"
-                placeholder="••••••••"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="confirm-password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                id="confirm-password"
-                name="confirm-password"
-                className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-400"
-                placeholder="••••••••"
-              />
-            </div>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            {registerFormData.map((item, i) => (
+              <div key={i}>
+                <label
+                  htmlFor={item.name}
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  {item.name}
+                </label>
+                <input
+                  type={item.type}
+                  id={item.name}
+                  name={item.name}
+                  value={formData[item.name]}
+                  className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-400"
+                  placeholder={item.placeholder}
+                  onChange={handleChange}
+                />
+              </div>
+            ))}
+
             <button
               type="submit"
               className="w-full py-3 px-4 bg-gradient-to-r from-teal-600 to-cyan-600 text-white font-semibold rounded-md shadow-md hover:from-teal-700 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition duration-300 ease-in-out"
